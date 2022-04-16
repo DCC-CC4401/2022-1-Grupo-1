1883512547d4f61b26788af04b693f5f235b61c5
Gabriel Rojas <gabo06123@gmail.com>
2022-04-16 02:13:46 -0400

    feat: create user model

--- depassist/urls.py
+++ depassist/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 # django
 from django.contrib import admin
 from django.urls import path
-
-# from django.urls import include
+from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("users.urls")),
 ]

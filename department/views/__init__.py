25f6cfc8b2b0161e960ebf39d8adabb4748884c9
Gabriel Rojas <gabo06123@gmail.com>
2022-06-14 22:56:21 -0400

    feat: create update announcement view

--- department/views/__init__.py
+++ department/views/__init__.py
@@ -1,11 +1,13 @@
 from .announcement_views import AnnouncementCreateView
 from .announcement_views import AnnouncementDetailView
 from .announcement_views import AnnouncementListView
+from .announcement_views import AnnouncementUpdateView
 from .visit_views import VisitCreateView
 
 __all__ = (
-    "VisitCreateView",
     "AnnouncementCreateView",
     "AnnouncementDetailView",
     "AnnouncementListView",
+    "AnnouncementUpdateView",
+    "VisitCreateView",
 )
